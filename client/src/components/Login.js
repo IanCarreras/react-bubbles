62c10de202b1ea8d0767284fdb65bccf7e291a9e
Ian <mus5002@aol.com>
2019-11-09 13:06:46 -0500

    created login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import api from '../utils/api'
+import styled from 'styled-components'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+const LoginDiv = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    margin: 5rem auto;
+`
+const Input = styled.input`
+    height: 2rem;
+    width: 80%;
+    margin: 1rem auto;
+`
+const Button = styled.button`
+    width: 20%;
+    margin: auto;
+`
 
-export default Login;
+const Login = props => {
+    const [error, setError] = useState()
+    const [user, setUser] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        api()
+            .post('/login', user)
+            .then(res => {
+                localStorage.setItem('token', res.data.payload)
+                props.history.push('/bubble-page')
+            })
+            .catch(err => {
+                setError(err.response.data.message)
+            })
+    }
+
+    return (
+        <LoginDiv onSubmit={handleSubmit}>
+            {error && <div className='error'>{error}</div>}
+
+            <Input type='text' name='username' placeholder='username' value={user.username} onChange={handleChange} />
+            <Input type='password' name='password' placeholder='password' value={user.password} onChange={handleChange} />
+
+            <Button type='submit'>Login</Button>
+        </LoginDiv>
+    )
+}
+
+export default Login
